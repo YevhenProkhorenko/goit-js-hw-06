a1f5de6002ff0e987c5aaecee925241a5188b4b6
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-07-10 12:51:04 +0300

    Task 3 is done

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,10 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const gallery = document.querySelector('.gallery')
+
+const imgItem = images.map(image => { 
+return `<li><img src="${image.url}" alt="${image.alt}"></li>`
+});
+gallery.insertAdjacentHTML("afterbegin", imgItem)
+console.log(imgItem);
