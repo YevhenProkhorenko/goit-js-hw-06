cb77b5ae9c0005f02e14b82451913b4c09c996b6
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-07-10 11:37:39 +0300

    complete second task

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,10 +1,9 @@
 const categories = document.querySelectorAll("#categories .item");
-console.log('Numbers of categories:', categories.length);
+console.log('Number of categories:', categories.length);
 [...categories].forEach((category) => {
     const elementTitle = category.querySelector('h2');
-    console.log('Title of category:', elementTitle.textContent);
+    console.log('Category:', elementTitle.textContent);
     
     const Number = category.querySelectorAll('li')    
-    console.log('The number of items in the category:', Number.length)
+    console.log('Elements:', Number.length)
 });
-
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listEl = document.querySelector('ul')
+
+for (let i = 0; i < ingredients.length; i += 1){
+  const addEl = document.createElement('li');
+  addEl.textContent = ingredients[i];
+  addEl.classList.add('item');
+  listEl.append(addEl);
+}
+
