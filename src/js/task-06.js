5410bcc137f9db3cc7c612f9712c33bf5d53da25
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-07-10 16:41:14 +0300

    in processing task 6

---
+++ src/js/task-06.js
@@ -0,0 +1,12 @@
+const textInput = document.querySelector("#validation-input");
+const borderInput = document.querySelector()
+const setTheFocus = () => {
+    textInput.focus();
+}
+const setTheBlur = () => {
+    textInput.blur();
+    if (textInput === textInput.dataset.length) {
+        
+    }
+}
+textInput.addEventListener("click", setTheFocus);
