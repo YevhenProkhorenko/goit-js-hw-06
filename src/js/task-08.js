680e98b2cd8636e40147d0bebd52ea66a7b559ed
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-07-10 21:32:06 +0300

    task 8 is done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+
+form.addEventListener("submit", pusheSubmit);
+
+function pusheSubmit(event) {
+  event.preventDefault();
+
+  if (event.currentTarget.email.value === "" || event.currentTarget.password.value === "") {
+    return alert("All fields must be filled");
+  }
+  else {
+      let user = {
+          Email: event.currentTarget.email.value,
+          Password: event.currentTarget.password.value,
+      }
+      console.log(user)
+  } 
+  event.currentTarget.reset();
+}
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,8 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+const button = document.querySelector(".change-color");
+const colorResult = document.querySelector(".color");
+colorResult.style.backgroundColor = button.addEventListener("click", getRandomHexColor);
