cb3a7d5c8fe13ddbcf728922123b9e1b0d6f025f
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-07-11 21:48:21 +0300

    finish task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -6,10 +6,6 @@ const body = document.querySelector("body");
 const button = document.querySelector(".change-color");
 let color = document.querySelector(".color");
 button.addEventListener("click", changeColor);
-// function changeColor() {
-//   body.style.background = getRandomHexColor();
-//   color.textContent = `${body.style.background}`;
-// }
 
 
 function changeColor() {
